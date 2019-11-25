9b22d2566a47f1e6435108d7c79aa43f8f5e190f
NathanFAIN <nathan.fain@epitech.eu>
2019-11-25 13:36:45 +0100

    adding run.sh

---
+++ Script.R
@@ -0,0 +1,44 @@
+library(XML)
+
+########################################################################################################
+################################# VERIFICATION DES DEUX REPERTOIRES ####################################
+########################################################################################################
+cat("1.Verification des deux repertoires:\n")
+#1.1.VSoil
+#1.1.1.VSoil Entrees
+cat("1.1.VSoil:\n")
+cat("1.1.1.VSoil Entrees:\n")
+
+#1.1.2.VSoil Sorties
+cat("1.1.2.VSoil Sorties:\n")
+
+#1.2.ESPAS
+#1.2.1.ESPAS Entrees
+cat("1.2.ESPAS:\n")
+cat("1.2.1.ESPAS Entrees:\n")
+
+#1.2.2.ESPAS Sorties
+cat("1.2.2.ESPAS Sorties:\n")
+
+########################################################################################################
+################################## VERIFICATION DES DONNEES D ENTRE ####################################
+########################################################################################################
+cat("2.Verification des donnees d'entrees:\n")
+#2.1.Validation du .ter
+cat("2.1.Validation du .ter:\n")
+
+#2.2.Validation des donnees de calibration
+cat("2.2.Validation des donnees de calibration:\n")
+
+#2.3.Masse et diametre
+cat("2.3.Masse et diametre:\n")
+
+########################################################################################################
+################################ VERIFICATION DES DONNEES DE SORTIE ####################################
+########################################################################################################
+cat("3.Verification des donnees de sorties:\n")
+#3.1.Potentiels matriciels
+cat("3.1.Potentiels matriciels:\n")
+
+#3.2.Observ_value
+cat("3.2.Observ_value:\n")
---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Vérification de la presence de la commande 'Rscript'
+RSCRIPT="$(which Rscript)"
+
+if [ "$RSCRIPT" = "" ]; then
+    echo "La commande Rscript n'est pas installé..."
+    sudo apt-get install r-base
+fi
+
+#Lancement du script
+Rscript Script.R
