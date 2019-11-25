a8e2520f581c75ba944526a47c8e380197352fb3
NathanFAIN <nathan.fain@epitech.eu>
2019-11-25 17:12:05 +0100

    fixing run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+CMD=Rscript
+
 #Vérification de la presence de la commande 'Rscript'
-RSCRIPT="$(which Rscript)"
+RSCRIPT="$(which $CMD)"
 
 if [ "$RSCRIPT" = "" ]; then
-    echo "La commande Rscript n'est pas installé..."
+    echo -e "\e[31mLa commande '$CMD' n'est pas installé...\e[0m"
     sudo apt-get install r-base
+    if [ "$?" != "0" ]; then
+        exit 1
+    fi
 fi
 
 #Lancement du script
+echo -e "\e[32mLancement du script!\e[0m\n\n"
 Rscript Script.R $1 $2
